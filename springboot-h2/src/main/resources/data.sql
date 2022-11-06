36266f2b4eed9c1feda2c85c127f3848b7f084e2
Ankur160689 <67689060+Ankur160689@users.noreply.github.com>
2022-11-06 12:19:51 +0530

    Update data.sql

--- springboot-h2/src/main/resources/data.sql
+++ springboot-h2/src/main/resources/data.sql
@@ -13,14 +13,14 @@ CREATE TABLE "employee"
    , "hire_date" DATE
    , "salary" NUMBER(8,2)
    , "manager_id" NUMBER(6)
-   , "department_id" NUMBER(4)   FOREIGN KEY REFERENCES department(department_id)
+   , "department_id" NUMBER(4)   
    ) ;
 INSERT INTO "employee" VALUES(1, 'Ankur Kumar', 'Das', 'ankurkrdas16@gmail.com', '+919874743078', '2021-09-25', '1000.0', 6, 12);
 INSERT INTO "employee" VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '+14064309812', '2021-09-16', '2000.0', 9, 19);
 INSERT INTO "employee" VALUES(3, 'Daniel', 'Smith', 'daniel.smith@gmail.com', '+9715289654925', '2022-06-16', '6000.0', 4, 5);
 INSERT INTO "employee" VALUES(4, 'Sanchari', 'Mehta', 'sanchari.mehta@gmail.com', '+619654309712', '2016-09-19', '5000.0', 6, 12);
-INSERT INTO "employee" VALUES(5, 'Lorry', 'Brown', 'lorry.brown@gmail.com', '+444569120456', '-2015-05-03', '4000.0', 22, 9);
+INSERT INTO "employee" VALUES(5, 'Lorry', 'Brown', 'lorry.brown@gmail.com', '+444569120456', '2015-05-03', '4000.0', 22, 9);
 INSERT INTO "department" VALUES(6, 'Information Technology', 12);
 INSERT INTO "department" VALUES(9, 'Sales', 19);
-INSERT INTO "department" VALUES(4, 'Makerting', 5);
+INSERT INTO "department" VALUES(4, 'Marketing', 5);
 INSERT INTO "department" VALUES(22, 'Human Resource', 9);
