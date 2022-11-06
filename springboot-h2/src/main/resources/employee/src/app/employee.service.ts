d7da0c54780233c69162c9798fc31057d1a58da0
Ankur160689 <67689060+Ankur160689@users.noreply.github.com>
2022-11-06 12:34:03 +0530

    Update employee.service.ts

--- springboot-h2/src/main/resources/employee/src/app/employee.service.ts
+++ springboot-h2/src/main/resources/employee/src/app/employee.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
 
-  private baseUrl = 'http://localhost:8080/springboot-h2/employee';
+  private baseUrl = 'http://localhost:8080/api/v1/';
 
   constructor(private http: HttpClient) { }
 
